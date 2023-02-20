6d26912ffc8b1e56c0c1d56f04bb92c0c8930f2e
Marcelo Vaquilema <rockerzega@gmail.com>
2023-02-19 22:38:12 -0500

    creacion de la vista

---
+++ app/src/main/java/com/sunayanpradhan/texttranslator/loginTmp.kt
@@ -0,0 +1,11 @@
+package com.sunayanpradhan.texttranslator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class loginTmp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_tmp)
+    }
+}
